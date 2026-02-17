c56fdcc939e47275be5474b2b8641a3362c93c79
octavemru <octavemoreau89@gmail.com>
2026-02-17 14:31:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 it document.addEventListener("DOMContentLoaded", () => {
 
-const supabase = window.supabase.createClient(
+var supabase = window.supabase.createClient(
   "https://ddzebzonhkghfzjkvuan.supabase.co",
   "sb_publishable_XEk9jtKJ__YwooQy-qU5ew_srAfLOnI"
 );
